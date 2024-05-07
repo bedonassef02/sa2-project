948dc8afccbec7766f64ed9905a4be0aaa6ef7fc
Amir202000160 <amirsamir225@gmail.com>
2024-05-07 21:48:03 +0300

    Login/SignUp

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -14,7 +14,29 @@ export const Login = ({ setIsAuthenticated }) => {
     const value = event.target.value;
     setInputs((values) => ({ ...values, [name]: value }));
   };
+//////////////////////////////////////////////////////////////////////////
 
+  const handleCh = (ev) => {
+    const name = ev.target.name;
+    const value = ev.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+////////////////////////////////////////////////////////////////////////
+{/*************************SIGNUP******************************************/}
+  const handleSignUp = async(e)=>{
+    await api
+    .userRegsiter(inputs)
+    .then((res) => {
+      console.log("user auth", res);
+    })
+    .catch((err) => {
+      setError(
+        err?.response?.data?.message ||
+          "The Authentication Service is under maintenance and it will be back soon!, Thanks for your patience!!"
+      );
+    })
+    .finally(() => setIsLoading(false));
+  }
   const handleSubmit = async (event) => {
     setIsLoading(true);
     event.preventDefault();
@@ -37,8 +59,10 @@ export const Login = ({ setIsAuthenticated }) => {
       .finally(() => setIsLoading(false));
   };
 
-  return (
+  return (<>
+  
     <form className={classes.formContainer} onSubmit={handleSubmit}>
+      <h1>Sign In</h1>
       <label className={classes.label}>
         <span className={classes.labelText}>Username:</span>
         <input
@@ -65,5 +89,44 @@ export const Login = ({ setIsAuthenticated }) => {
       {error && <p className={classes.errorMsg}>{error}</p>}
       {isLoading && <p>isLoading ... </p>}
     </form>
+    <br>
+    </br>
+  {/*******************************************************************/}
+    <form className={classes.formContainer} onSubmit={handleSignUp}>
+      <h1>Sign Up</h1>
+      <label className={classes.label}>
+        <span className={classes.labelText}>Username:</span>
+        <input
+          className={classes.input}
+          type="text"
+          name="email"
+          onChange={handleCh}
+          autoComplete="off"
+          required
+        />
+        <span className={classes.labelText}>name:</span>
+        <input
+          className={classes.input}
+          type="text"
+          name="name"
+          onChange={handleCh}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label className={classes.label}>
+        <span className={classes.labelText}>Password:</span>
+        <input
+          className={classes.input}
+          type="password"
+          name="password"
+          onChange={handleCh}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <input className={classes.submit} type="submit" />
+      
+    </form></>
   );
 };
